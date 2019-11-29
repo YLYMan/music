9ddacba4b306492689017d822b0b3355f2bbcdfe
YMan <yly3639@163.com>
2019-11-29 17:42:57 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <m-header></m-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import MHeader from 'components/m-header/m-header'
+
+  export default {
+    name: 'App',
+    components: {
+      MHeader
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable"
+
+  #app
+    color: $color-theme
+</style>
